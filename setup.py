49b9b77f3163bc1a574259026a4545e0cdf601ab
Adrian Roß <adrian.ross@ross-agentur.de>
2024-07-19 22:47:20 +0200

    Create setup.py for this package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="pyframebuffer",
+      version="1.0",
+      author="Adrian Roß",
+      author_email="adrian.ross@ross-agentur.de",
+      description="A linux framebuffer primitives library written in C and Python.",
+      download_url="https://github.com/RossAdrian/pyframebuffer/archive/refs/heads/main.zip",
+      license="MIT",
+      long_description="A linux library written in C and Python to draw with graphic primitives to the framebuffer.",
+      maintainer="Adrian Roß",
+      maintainer_email="adrian.ross@ross-agentur.de",
+      url="https://github.com/RossAdrian/pyframebuffer"
+)
