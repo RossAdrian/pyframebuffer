7a88d97709791589561b6bef856aeebbcaaa212d
Adrian Roß <adrian.ross@ross-agentur.de>
2024-07-22 17:13:08 +0200

    Fix recent workflow fail and react on latest flake8 lint warnings

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 import platform
 
+
 def check_platform():
     if platform.system() != "Linux":
         raise RuntimeError("This package does only support Linux")
     print("Installing on platform: ", platform.system())
+
+
+check_platform()
 
 setup(name="pyframebuffer",
       version="1.0",
@@ -16,6 +20,4 @@ setup(name="pyframebuffer",
       long_description="A linux library written in C and Python to draw with graphic primitives to the framebuffer.",
       maintainer="Adrian Roß",
       maintainer_email="adrian.ross@ross-agentur.de",
-      url="https://github.com/RossAdrian/pyframebuffer",
-      cmdclass={'install': check_platform}
-     )
+      url="https://github.com/RossAdrian/pyframebuffer")
