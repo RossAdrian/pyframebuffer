142bd1c71511f50c0d2a6d01fd0c2a85c1a659fc
Adrian Roß <adrian.ross@ross-agentur.de>
2024-07-27 17:37:33 +0000

    Add vertical line drawing method to the Python module declaration

--- native/module_pyfb.c
+++ native/module_pyfb.c
@@ -108,6 +108,38 @@ static PyObject* pyfunc_pyfb_sdrawHorizontalLine(PyObject* self, PyObject* args)
     return PyLong_FromLong(exitcode);
 }
 
+/**
+ * Python wrapper for the pyfb_sdrawVerticalLine function.
+ * 
+ * @param self The function
+ * @param args The arguments, expecting long of the fbnum, long of the x coordinate, long of the y coordinate, long of the len coordinate and long for the color
+ * 
+ * @return Just a 0
+ */
+static PyObject* pyfunc_pyfb_sdrawVerticalLine(PyObject* self, PyObject* args) {
+    unsigned char fbnum_c;
+    unsigned long int x;
+    unsigned long int y;
+    unsigned long int len;
+    uint32_t color_val;
+
+    if(!PyArg_ParseTuple(args, "bkkkI", &fbnum_c, &x, &y, &len, &color_val)) {
+        PyErr_SetString(PyExc_TypeError, "Expecting arguments of type (byte, long, long, long, long)");
+        return NULL;
+    }
+
+    // now parse the color
+    struct pyfb_color color;
+    pyfb_initcolor_u32(&color, color_val);
+
+    // and invoke the target function
+    pyfb_sdrawVerticalLine((uint8_t)fbnum_c, x, y, len, &color);
+
+    // ready
+    int exitcode = 0;
+    return PyLong_FromLong(exitcode);
+}
+
 // The module def
 
 /**
@@ -118,6 +150,7 @@ static PyMethodDef pyfb_methods[] = {
     {"pyfb_close", pyfunc_pyfb_close, METH_VARARGS, "Framebuffer close function"},
     {"pyfb_setPixel", pyfunc_pyfb_ssetPixel, METH_VARARGS, "Draw a pixel on the framebuffer"},
     {"pyfb_drawHorizontalLine", pyfunc_pyfb_sdrawHorizontalLine, METH_VARARGS, "Draw a horizontal line on the framebuffer"},
+    {"pyfb_drawVerticalLine", pyfunc_pyfb_sdrawVerticalLine, METH_VARARGS, "Draw a vertical line on the framebuffer"},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef module__pyfb = {PyModuleDef_HEAD_INIT,
